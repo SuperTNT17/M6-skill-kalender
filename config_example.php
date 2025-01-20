5c612834687c36340d3e38400b5e88e75e493760
Robin Lemmers <38210@ma-web.nl>
2025-01-20 14:17:37 +0100

    db verbinding

---
+++ config_example.php
@@ -0,0 +1,25 @@
+<?php
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', '...' );
+
+/** Database username */
+define( 'DB_USER', '...' );
+
+/** Database password */
+define( 'DB_PASSWORD', '...' );
+
+/** Database hostname */
+define( 'DB_HOST', '...' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', '...' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+define( 'SERVERNAME', '...' );
+
+
+?>
